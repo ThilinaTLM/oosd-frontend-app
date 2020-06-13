06bd69f0ed09741fb1ce591a45b5f35e7e69ddf6
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-06-14 00:20:03 +0530

    06/14 Col

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,31 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+<v-app>
+  <v-toolbar
+  class="app-bar"
+  >
+    <Header />
+  </v-toolbar>
+  <v-content>
+  <v-layout>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Veritatis minus optio, quis dolore ad temporibus ullam provident
+          iste modi fugit! Nostrum blanditiis eos veniam, odit eligendi 
+          porro illo adipisci id?</p>
+  </v-layout>
+    
+  </v-content>
+</v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld,
+    Header
   },
 
   data: () => ({
@@ -59,3 +33,12 @@ export default Vue.extend({
   }),
 });
 </script>
+
+<style>
+.app-bar {
+  overflow: hidden;
+  padding: 0;
+  margin: 0;
+  height: auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container class="container">
+      <v-row>
+         <v-col class="pa-1">
+            <v-img
+            :src="require('../assets/main-gov.png')"
+            contain
+            height="80px"/>
+        </v-col>
+        <v-col class="pa-3">
+            <v-row> 
+            <h1>Galle District</h1>
+            </v-row>
+            <v-row> 
+            <h2>Public Grievances </h2>
+            </v-row>
+        </v-col>
+        <v-col class="pa-1">
+            <v-row>
+                <h3>Visit Galle District Secretariat</h3>
+            </v-row>
+            <v-row>
+              <v-col>
+                  Sinhala
+            </v-col>
+            <v-col>
+                Tamil
+            </v-col>
+            </v-row>
+        </v-col>
+      </v-row> 
+  </v-container>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "Header"  
+})
+</script>
+
+<style scopped>
+.container {  
+    background-color:#F9F6F3;
+}
+
+h1 {
+    margin: 0;
+    font-size: 1.5em;
+}
+
+h2{
+    margin:0;
+    font-size: 1em;
+}
+</style>

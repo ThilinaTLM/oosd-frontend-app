4eb48bb9dff942cd28dc3100513c73d437b0890b
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-06-14 00:20:03 +0530

    06/14 Col
    (cherry picked from commit 06bd69f0ed09741fb1ce591a45b5f35e7e69ddf6)
    
    Few changes for App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,44 @@
 <template>
-<v-app>
-  <v-toolbar
-  class="app-bar"
-  >
-    <Header />
-  </v-toolbar>
-  <v-content>
-  <v-layout>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-         Veritatis minus optio, quis dolore ad temporibus ullam provident
-          iste modi fugit! Nostrum blanditiis eos veniam, odit eligendi 
-          porro illo adipisci id?</p>
-  </v-layout>
-    
-  </v-content>
-</v-app>
+    <v-app>
+        <v-toolbar
+                class="app-bar"
+        >
+            <Header />
+        </v-toolbar>
+        <v-content>
+            <v-layout>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veritatis minus optio, quis dolore ad temporibus ullam provident
+                    iste modi fugit! Nostrum blanditiis eos veniam, odit eligendi
+                    porro illo adipisci id?</p>
+            </v-layout>
+
+        </v-content>
+    </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Header from './components/Header.vue';
+    import Vue from 'vue';
+    import Header from './components/Header.vue';
 
-export default Vue.extend({
-  name: 'App',
+    export default Vue.extend({
+        name: 'App',
 
-  components: {
-    Header
-  },
+        components: {
+            Header
+        },
 
-  data: () => ({
-    //
-  }),
-});
+        data: () => ({
+            //
+        }),
+    });
 </script>
 
 <style>
-.app-bar {
-  overflow: hidden;
-  padding: 0;
-  margin: 0;
-  height: auto;
-}
+    .app-bar {
+        overflow: hidden;
+        padding: 0;
+        margin: 0;
+        height: auto;
+    }
 </style>
